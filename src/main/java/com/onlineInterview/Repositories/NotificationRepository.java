82e0f0c2a52f8be07d2e8179a4843896f5c5927b
Abdelrahman Lotfy Mohammed <36973578+AbdoFCI@users.noreply.github.com>
2018-12-16 18:50:25 +0200

    Deleting Notification Repository

--- src/main/java/com/onlineInterview/Repositories/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onlineInterview.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.onlineInterview.Entities.Notification;
-
-public interface NotificationRepository extends CrudRepository<Notification, Integer> {
-
-}
